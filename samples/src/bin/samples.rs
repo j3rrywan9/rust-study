84e283eb2667e43080e8a34287adc8971908a074
Jerry Wang <j3rry.wan9@gmail.com>
2021-09-12 17:37:56 -0700

    split samples

--- samples/src/bin/samples.rs
+++ samples/src/bin/samples.rs
@@ -1,22 +1,6 @@
-use samples::{color, gcd, show};
-use std::env;
-use std::fs;
-use std::str::FromStr;
+use samples::{color, show};
 
 fn main() {
-    let url = "https://www.rust-lang.org/";
-    let output = "rust.md";
-    println!("Size of output: {:?}", std::mem::size_of_val(output));
-
-    println!("Fetching URL: {}", url);
-    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
-
-    println!("Converting HTML to markdown...");
-    let md = html2md::parse_html(&body);
-
-    fs::write(output, md.as_bytes()).unwrap();
-    println!("Converted markdown has been saved in {}.", output);
-
     let rgb = color;
     show(rgb);
 
@@ -24,22 +8,4 @@ fn main() {
 
     let c = |_: &str| { (1, 2, 3) };
     show(c);
-
-    let mut numbers = Vec::new();
-
-    for arg in env::args().skip(1) {
-        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
-    }
-
-    if numbers.len() == 0 {
-        eprint!("Usage: gcd NUMBER ...");
-        std::process::exit(1);
-    }
-
-    let mut d = numbers[0];
-    for m in &numbers[1..] {
-        d = gcd(d, *m);
-    }
-
-    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
