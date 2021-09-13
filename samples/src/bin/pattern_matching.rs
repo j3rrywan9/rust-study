75e8efbed82fe6d69b2459316c9343c99e4d69c8
Jerry Wang <j3rry.wan9@gmail.com>
2021-09-12 22:03:03 -0700

    add an example for pattern matching

---
+++ samples/src/bin/pattern_matching.rs
@@ -0,0 +1,44 @@
+#![allow(unused)]
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum TimeUnit {
+    Seconds, Minutes, Hours, Days, Months, Years,
+}
+
+impl TimeUnit {
+    fn plural(self) -> &'static str {
+        match self {
+            TimeUnit::Seconds => "seconds",
+            TimeUnit::Minutes => "minutes",
+            TimeUnit::Hours => "hours",
+            TimeUnit::Days => "days",
+            TimeUnit::Months => "months",
+            TimeUnit::Years => "years",
+        }
+    }
+
+    fn singular(self) -> &'static str {
+        self.plural().trim_end_matches('s')
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum RoughTime {
+    InThePast(TimeUnit, u32),
+    JustNow,
+    InTheFuture(TimeUnit, u32),
+}
+
+fn rough_time_to_english(rt: RoughTime) -> String {
+    match rt {
+        RoughTime::InThePast(units, count) => format!("{} {} ago", count, units.plural()),
+        RoughTime::JustNow => format!("just now"),
+        RoughTime::InTheFuture(units, count) => format!("{} {} from now", count, units.plural()),
+    }
+}
+
+fn main() {
+    let rt = RoughTime::InTheFuture(TimeUnit::Months, 2);
+
+    println!("{}", rough_time_to_english(rt));
+}
